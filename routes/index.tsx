e655c7b9c0b44f13acbc8a87e8423178e70d2686
reddhouse <reddhouse@gmail.com>
2023-04-05 13:46:21 -0700

    Add copy at homepage

--- routes/index.tsx
+++ routes/index.tsx
@@ -24,9 +24,9 @@ export default function Home() {
               <span class="home__tagline--emphasized">our nation.</span>
             </div>
             <div class="home__sub-tagline">
-              Join the first cooperatively owned & managed political party,
-              built from the ground-up for member participation and complete
-              transparency.
+              Join the member-owned, member-managed political cooperative that
+              challenges everything you've come to expect from your political
+              party.
             </div>
           </div>
           <div class="home__flex-col-image">
@@ -38,7 +38,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div class="home__flex-row-details">
+        <div class="home__flex-row-challenge">
           <div class="home__flex-col-image">
             <img
               class="home__lemon-svg"
@@ -46,14 +46,72 @@ export default function Home() {
               alt="the fresh logo: a sliced lemon dripping with juice"
             />
           </div>
-          <div class="home__flex-col-details">
-            <div class="home__details">
-              <span>Another blurb goes here.</span>
+          <div class="home__flex-col-challenge">
+            <div class="home__challenge">
+              <span>Two sides of the same coin</span>
             </div>
-            <div class="home__sub-details">
-              Join the first cooperatively owned & managed political party,
-              built from the ground-up for member participation and complete
-              transparency.
+            <div class="home__sub-challenge">
+              Based on values, and visions for the future, traditional political
+              parties may appear as different as night and day. In reality,
+              these parties are united in their efforts to auction lawmaking to
+              the highest bidders. As wealthy companies and individuals are the
+              only entities that can afford access, policies and reforms that
+              would deliver the largest benefits for average Americans, rarely
+              see the light of day.
+            </div>
+          </div>
+        </div>
+
+        <div class="home__flex-row-tagline">
+          <div class="home__flex-col-tagline">
+            <div class="home__tagline">
+              <span>Corruption-proof by default</span>
+            </div>
+            <div class="home__sub-tagline">
+              The Cooperative Party eliminates the influence of money in
+              politics by cutting out the largest expenses for political
+              parties; the campaign. No campaigns = No advertising = No
+              fundraising = No favors owed. Cooperative Party candidates for
+              office are pre-selected by party members, and do not need to
+              compete with one another to win primaries or prove their
+              fundraising prowess.
+            </div>
+          </div>
+          <div class="home__flex-col-image">
+            <img
+              class="home__lemon-svg"
+              src="/logo.svg"
+              alt="the fresh logo: a sliced lemon dripping with juice"
+            />
+          </div>
+        </div>
+
+        <div class="home__flex-row-challenge">
+          <div class="home__flex-col-image">
+            <img
+              class="home__lemon-svg"
+              src="/logo.svg"
+              alt="the fresh logo: a sliced lemon dripping with juice"
+            />
+          </div>
+          <div class="home__flex-col-challenge">
+            <div class="home__challenge">
+              <span>Cooperative by design</span>
+            </div>
+            <div class="home__sub-challenge">
+              While traditional political parties seek to find like-minded
+              people to rally behind various causes, the Cooperative Party takes
+              the opposite approach. We start with members from different
+              ideological backgrounds, and identify the problems they
+              collectively agree are worth solving.
+              <br />
+              <br />
+              Cooperative describes the process we use to find common ground,
+              but also the structure of the organization. Members define the
+              party's priorities, members control the party's finances, and
+              members choose the representatives we send to congress. Even
+              elements of this website are edited and approved through
+              democratic consensus.
             </div>
           </div>
         </div>
